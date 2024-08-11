42ee76ab953dfb659c7377b9b37b486314e3f809
David Chapman <talk2dc@gmail.com>
2024-08-10 21:09:24 -0400

    Fix hiding directories when using ** with fzf

--- zsh/fzf.sh
+++ zsh/fzf.sh
@@ -1,7 +1,14 @@
-export FZF_CTRL_T_OPTS="
-  --walker-skip .git,node_modules,anaconda3,elasticsearch-*
-"
 export FZF_DEFAULT_OPTS="--ansi"
 export FZF_DEFAULT_COMMAND='fd --type file --color=always'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow . "$1"
+}
+
+# Use fd to generate the list for path completion
+_fzf_compgen_path() {
+  fd --type f --hidden --follow . "$1"
+}
